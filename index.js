9197f5884686f009a50f5f0f60c57fd5fda6773f
Arko <arkoroyb@gmail.com>
2022-10-25 02:45:00 +0600

    some dynamic get request  declared

--- index.js
+++ index.js
@@ -14,8 +14,20 @@ app.get('/course-category', (req, res) => {
     res.send(categories);
 })
 
+app.get('/course-category/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCategory = course.filter(n => n.category_id === id);
+    res.send(selectedCategory);
+})
+
 app.get('/course', (req,res) => {
     res.send(course);
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(n => n._id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
