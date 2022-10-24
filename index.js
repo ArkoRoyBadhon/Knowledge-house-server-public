60b3036e132af201a3e6265e4aba29a1e24c7f85
Arko <arkoroyb@gmail.com>
2022-10-25 02:46:50 +0600

    cors install and implement in our app

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
+var cors = require('cors')
+
+
+app.use(cors())
 
 
 const categories = require('./categories.json');
@@ -31,6 +35,6 @@ app.get('/course/:id', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('Dragon News Server running on port ');
+    console.log('Server running on port ', PORT);
 })
 
