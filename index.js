da76fc1acb6bc0e967177cab75bf9a45a724eddf
Arko <arkoroyb@gmail.com>
2022-10-26 02:17:42 +0600

    huge change after revising the assignment requirements

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ var cors = require('cors')
 app.use(cors())
 
 
-const categories = require('./categories.json');
-const course = require('./course.json')
+const categories = require('./categoryList.json');
+// const course = require('./course.json')
+const courseDetail = require('./courseDetails.json');
 
 app.get('/', (req, res) => {
     res.send('Course API Running');
@@ -18,23 +19,32 @@ app.get('/course-category', (req, res) => {
     res.send(categories);
 })
 
-app.get('/course-category/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedCategory = course.filter(n => n.category_id === id);
-    res.send(selectedCategory);
+app.get('/course-detail', (req, res) => {
+    res.send(courseDetail);
 })
 
-app.get('/course', (req,res) => {
+app.get('/course-detail/:id', (req, res) => {
+        const id = req.params.id;
+        const selectedCategory = courseDetail.filter(course => course.id === id);
+        res.send(selectedCategory);
+    
+
+})
+
+app.get('/course', (req, res) => {
     res.send(course);
 })
 
 app.get('/course/:id', (req, res) => {
+
     const id = req.params.id;
     const selectedCourse = course.find(n => n._id === id);
     res.send(selectedCourse);
+
+
 })
 
 app.listen(port, () => {
-    console.log('Server running on port ', PORT);
+    console.log('Server running on port ', port);
 })
 
